f98223aac61a5875dfd26855b39b33fd5c8e8223
gbstrom <gbstrom@gmail.com>
2022-08-17 16:19:02 -0400

    created navbar files & added dummy code

--- src/components/main-view/main-view.js
+++ src/components/main-view/main-view.js
@@ -1,1 +1,11 @@
 import React from 'react';
+import NavBar from '../navbar/navbar';
+
+export default function MainView() {
+  return (
+    <>
+      <NavBar />
+      <div>This is the modest beginning of the AGDb.</div>
+    </>
+  )
+}
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function NavBar() {
+  return (
+    <div>This here is gonna be a navbar!</div>
+  )
+}
