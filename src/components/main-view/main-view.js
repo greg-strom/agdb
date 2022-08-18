a369dc33c302d77d4198b628266c985ff80c4c02
gbstrom <gbstrom@gmail.com>
2022-08-18 13:35:37 -0400

    added slightly more elaborate dummy content

--- src/components/main-view/main-view.js
+++ src/components/main-view/main-view.js
@@ -5,7 +5,17 @@ export default function MainView() {
   return (
     <>
       <NavBar />
-      <div>This is the modest beginning of the AGDb.</div>
+      <div className="main-view">
+        {/* In the fulness of time the following will be replaced with something more or less like this:
+          <div className="main-view">
+            {pieces.map(piece => <div key={piece._id}>{piece.title}</div>)}
+          </div>
+        */}
+        <div>Stockhausen, "Stimmung"</div>
+        <div>Feldman, "Piano and String Quartet"</div>
+        <div>Cage, "Sonatas and Interludes"</div>
+        <div>Stockhausen, "Unsichtbare Chöre"</div>
+      </div>
     </>
   )
 }
