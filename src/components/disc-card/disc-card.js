8171966ee3354cbc9a6ef6e733ddd96760c24e3f
gbstrom <gbstrom@gmail.com>
2022-08-20 14:48:44 -0400

    tikered with disc card code

--- src/components/disc-card/disc-card.js
+++ src/components/disc-card/disc-card.js
@@ -3,9 +3,13 @@ import DiscView from '../disc-view/disc-view'
 
 export default function DiscCard(props) {
   const { disc, onDiscClick } = props;
-  return <div className="disc-card" onClick={() => onDiscClick(disc)}> <img src={disc.imagepath} alt="disc cover"></img><br />{disc.recordlabel + " " + disc.catalognumber} </div>;
+  return (
+    <div className="disc-card" onClick={() => onDiscClick(disc)}>
+      <img src={disc.imagepath} alt="disc cover art"></img>
+      <br />
+      {disc.recordlabel}
+      <br />
+      {disc.catalognumber}
+    </div>
+  )
 }
-
-
-//onDiscClick(disc)
-//{() => { onDiscClick(disc); }}
