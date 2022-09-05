95acc38f874453d03294c63e4036fba7dd8ff409
gbstrom <gbstrom@gmail.com>
2022-09-05 14:25:55 -0400

    tidied code in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import MainView from './components/main-view/main-view'
-import TestComponent from './components/test-component/test-component'
 import './App.css';
-import AppRouter from './AppRouter'
 
 function App() {
   return (
-    // <AppRouter />
     <MainView />
-    // <TestComponent />
   );
 }
 
