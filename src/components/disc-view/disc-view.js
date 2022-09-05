fc616c21e5a52b883dce74d42b04df1a04f9eb5d
gbstrom <gbstrom@gmail.com>
2022-09-05 13:56:16 -0400

    fixed up discview so that it has its own api call

--- src/components/disc-view/disc-view.js
+++ src/components/disc-view/disc-view.js
@@ -1,63 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import { useParams } from "react-router-dom";
+import { LinkContainer } from 'react-router-bootstrap';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 import './disc-view.scss';
 
+const APIURL = 'https://avant-garde-api.herokuapp.com/';
+
 export default function DiscView(props) {
-  const { disc, onBackClick } = props;
+  const { onBackClick } = props;
+
+  const { discId } = useParams();
+
+  const [disc, setDisc] = useState(null)
+
+  useEffect(() => {
+    axios.get(`${APIURL}discs/${discId}`).then((response) => {
+      setDisc(response.data);
+    })
+  }, [])
+
+
+  // Note to self: for reasons sort of explained here -- https://stackoverflow.com/questions/54069253/the-usestate-set-method-is-not-reflecting-a-change-immediately
+  // -- the following useEffect is necessary in order for the data received from the axios API calls above to get fixed properly as the values of the useState constants.
+  useEffect(() => { console.log(JSON.stringify(disc)) }, [disc])
+
   return (
     <Row className="justify-content-md-start">
       <Col md={4}>
-        <Card>
-          <Card.Img variant="top" className="disc-cover-art" alt="disc cover art" src={disc.imagepath} />
+        <Card className="border-0">
+          <Card.Img variant="top" className="disc-cover-art" alt="disc cover art" src={disc && disc.imagepath} />
+          <Card.Subtitle className="record-data">{disc && disc.recordlabel} - {disc && disc.catalognumber}</Card.Subtitle>
+          <Card.Body>
+            <LinkContainer to={`/composers/${disc && disc.recordings[0].composer.surname}`} className="link">
+              <Card.Title>{disc && disc.recordings[0].composer.givenname} {disc && disc.recordings[0].composer.surname}</Card.Title>
+            </LinkContainer>
+            <div className="recording-info-wrapper">
+              {disc && disc.recordings.map((recording) => (
+                <div key={recording._id} className="recording-info">
+                  <Card.Subtitle className="piece-title-and-year"><span className="piece-title-span">{recording.piece.title}</span> <span className="piece-year-span">({recording.piece.year}</span>)</Card.Subtitle>
+                  <Card.Subtitle className="performers-info">{recording.performers.map((performer, index) => (
+                    <span key={index}>{performer + (index === recording.performers.length - 1 ? '' : ', ')} </span>
+                  ))}
+                  </Card.Subtitle>
+                </div>
+              ))}
+
+            </div>
+          </Card.Body>
         </Card>
       </Col>
       <Col md={8}>
         <Card className="border-0">
           <Card.Body>
-            <Card.Title>{disc.recordings[0].composer.givenname} {disc.recordings[0].composer.surname}</Card.Title>
-            {disc.recordings.map((recording) => (
-              <div key={recording._id}>
-                <Card.Subtitle>{recording.piece.title} ({recording.piece.year})</Card.Subtitle>
-                <Card.Subtitle>{recording.performers.map((performer, index) => (
-                  <span key={index}>{performer + (index === recording.performers.length - 1 ? '' : ', ')} </span>
-                ))}
-                </Card.Subtitle>
-                <br />
-              </div>
-            ))}
 
-            {/* <Link to={`/directors/${movie.Director.Name}`}>
-            <Button variant="link">Director</Button>
-          </Link>
-          <Link to={`/genres/${movie.Genre.Name}`}>
-            <Button variant="link">Genre</Button>
-          </Link> */}
-            <Card.Text className="post">{disc.comments}</Card.Text>
-            <Card.Subtitle className="record-label">{disc.recordlabel} - {disc.catalognumber}</Card.Subtitle>
+            {disc && disc.comments
+              ?
+              <Card.Text className="post">{disc && disc.comments}</Card.Text>
+              :
+              <Card.Text className="post">In the fullness of time, I will post some commentary about this CD here.</Card.Text>
+            }
+
             <Button className="back-button" variant="primary" type="submit" onClick={() => { onBackClick(null); }}>Back</Button>
           </Card.Body>
         </Card>
       </Col>
+
     </Row>
   )
 }
-
-/* <div className="disc-view">
-      <div className="disc-image">
-        <img alt="" src={disc.imagepath} />
-      </div>
-      <div className="disc-title">
-        <span className="label">Record Label: </span>
-        <span className="value">{disc.recordlabel}</span>
-      </div>
-      <div className="disc-catalognumber">
-        <span className="label">Catalog Number: </span>
-        <span className="value">{disc.catalognumber}</span>
-      </div>
-      <button onClick={() => { onBackClick(null); }}>Go Back</button>
-    </div> */
