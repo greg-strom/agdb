7c50e4c5f41fd089dcf7db1f75d7791543baf5a7
gbstrom <gbstrom@gmail.com>
2022-09-05 14:09:46 -0400

    fixed some code relating to links in disc card

--- src/components/disc-card/disc-card.js
+++ src/components/disc-card/disc-card.js
@@ -3,6 +3,7 @@ import DiscView from '../disc-view/disc-view'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom";
+import { LinkContainer } from 'react-router-bootstrap';
 
 import './disc-card.scss';
 
@@ -10,15 +11,17 @@ export default function DiscCard(props) {
   const { disc } = props;
   return (
     <Card className="h-100 m-25 border-0 disc-card">
-      <Link to={`/discs/${disc._id}`}>
-        <Card.Img className="disc-cover-art" src={disc.imagepath} alt="disc cover art" />
+      <Link to={`/discs/${disc._id}`} className="link">
+        <Card.Img src={disc.imagepath} alt="disc cover art" />
       </Link>
-      <Card.Body>
-        <Card.Title>{disc.recordings[0].composer.givenname} {disc.recordings[0].composer.surname}</Card.Title>
+      <Card.Body className="card-body">
+        <LinkContainer to={`/composers/${disc.recordings[0].composer.surname}`} className="link">
+          <Card.Title>{disc.recordings[0].composer.givenname} {disc.recordings[0].composer.surname}</Card.Title>
+        </LinkContainer>
         {disc.recordings.map((recording, index) => (
           <div key={recording._id}>
 
-            <Card.Subtitle>{recording.piece.title} ({recording.piece.year})</Card.Subtitle>
+            <Card.Text className="piece-details"><span className="piece-title">{recording.piece.title}</span> ({recording.piece.year})</Card.Text>
             {/* {recording.performers.map((performer, index) => (
               <div><span key={index}>{performer + (index === recording.performers.length - 1 ? '' : ', ')} </span><br></br></div>
               // <div key={index}>
@@ -28,7 +31,7 @@ export default function DiscCard(props) {
           </div>
         ))}
       </Card.Body>
-    </Card>
+    </Card >
   )
 }
 
