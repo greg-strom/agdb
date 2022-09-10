7162514084cc4521c561eac8751ca3aa1bb2cf42
gbstrom <gbstrom@gmail.com>
2022-09-10 08:31:00 -0400

    added clarification to about section

--- src/components/about-view/about.js
+++ src/components/about-view/about.js
@@ -14,7 +14,7 @@ export default function About(props) {
         <li className="list-item">to use the collection of about 3000 experimental classical music CDs that I’ve been assembling since the 1990s to do something creative, maybe semi-autobiographical, and potentially useful and informative for other people.</li>
       </ul>
       <p>I’ll be writing up little essays to accompany the CDs and composers I put on display here, which I hope it will be fun for people to read. I should maybe remark that although I know a fair amount about this music,
-        I am not at all a professional musician. I did study a bit of music theory when I was younger (enough that terms like “Neapolitan chord,” “Picardy third” and “hemiola” are still in my active vocabulary), and
+        I am not at all a professional musician or music writer. I did study a bit of music theory when I was younger (enough that terms like “Neapolitan chord,” “Picardy third” and “hemiola” are still in my active vocabulary), and
         I briefly fantasized about becoming a composer, but I sort of half chickened out, half got really interested in philosophy instead, so I took a different path while cultivating an intense amateur interest in
         contemporary composition. I like to think I’m the sort of person these composers fantasize about reaching: a relatively ordinary person, who just happens to find their music fascinating. I think this music has
         the potential to appeal to many more people than the specialists, and that’s something I hope to show by how I write about it.
